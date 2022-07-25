ed792cdb82a4409cf1fcf7c59e7681b31d1dc92c
NikarashiHatsu <yourlovelydev@gmail.com>
2022-07-25 19:16:00 +0700

    chore: implement new componetn

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,38 +13,23 @@
         @csrf
         <div class="card-body">
             <h2 class="card-title text-center mb-4">Masuk ke akun Anda</h2>
-            <div class="mb-3">
-                <label class="form-label">Alamat Email</label>
-                <input type="email" class="form-control" placeholder="Masukkan Email" name="email" />
-            </div>
-            <div class="mb-2">
-                <label class="form-label">
-                    Kata Sandi
-                    @if (Route::has('password.request'))
-                        <span class="form-label-description">
-                            <a href="{{ route('password.request') }}">Lupa kata sandi?</a>
-                        </span>
-                    @endif
-                </label>
-                <div class="input-group input-group-flat">
-                    <input name="password" type="password" class="form-control" placeholder="Kata Sandi" autocomplete="off">
-                    <span class="input-group-text">
-                        <a href="javascript:void(0)" class="link-secondary" title="Show password" data-bs-toggle="tooltip">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                <circle cx="12" cy="12" r="2" />
-                                <path d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7" />
-                            </svg>
-                        </a>
-                    </span>
-                </div>
-            </div>
-            <div class="mb-2">
-                <label class="form-check">
-                    <input type="checkbox" class="form-check-input" name="remember_name" />
-                    <span class="form-check-label">Ingat Saya</span>
-                </label>
-            </div>
+            <x-tabler::form.input.text
+                label="Alamat Email"
+                name="email"
+                placeholder="Masukkan Email"
+                :value="old('email')"
+                required
+            />
+            <x-tabler::form.input.password
+                label="Kata Sandi"
+                name="password"
+                placeholder="Masukkan Kata Sandi"
+                required
+            />
+            <x-tabler::form.input.checkbox
+                label="Ingat saya"
+                name="remember_me"
+            />
             <div class="form-footer">
                 <button type="submit" class="btn btn-primary w-100">Masuk</button>
             </div>
