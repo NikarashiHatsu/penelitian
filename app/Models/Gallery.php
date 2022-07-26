417ee12ca0b8cf77efac95a86fb9b10042558961
NikarashiHatsu <yourlovelydev@gmail.com>
2022-07-26 17:43:00 +0700

    fix: asset not found

--- app/Models/Gallery.php
+++ app/Models/Gallery.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Gallery extends Model
 {
@@ -19,6 +21,13 @@ class Gallery extends Model
                 $model->file = request()->file('file')->storePublicly('galleries');
             }
         });
+    }
+
+    protected function file(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => Storage::url($value),
+        );
     }
 
     protected $fillable = [
